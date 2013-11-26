33b697276b813ad4a749f0a565f0ed69a5056ede
checherman <geminis_che@hotmail.com>
2013-11-26 12:45:22 -0600

    Tes: tree

--- application/controllers/TES/enrolamiento.php
+++ application/controllers/TES/enrolamiento.php
@@ -9,10 +9,10 @@ class Enrolamiento extends CI_Controller {
 		$data["title"]="TES";
 		$data["titulo"]="Enrolamiento";
 		
-		$this->template->write_view('header',DIR_TES.'/header.php',$data);
-		$this->template->write_view('menu',DIR_TES.'/menu.php');
-		$this->template->write_view('content',DIR_TES.'/enrolamiento/enrolamiento.php');
-		$this->template->write_view('footer',DIR_TES.'/footer.php');	
+		//$this->template->write_view('header',DIR_TES.'/header.php');
+		//$this->template->write_view('menu',DIR_TES.'/menu.php');
+		$this->template->write_view('content',DIR_TES.'/enrolamiento/enrolamiento.php',$data);
+		//$this->template->write_view('footer',DIR_TES.'/footer.php');	
 		$this->template->render();
 	}
 }
