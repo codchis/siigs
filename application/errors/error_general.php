8173b3e69f1cf92ba774aec51c587f0ff9e801d5
p4scu41 <pereznango@gmail.com>
2014-01-09 12:37:25 -0600

    Se agrega las nuevas variables js y ajustaAncho a las vistas de errores

--- application/errors/error_general.php
+++ application/errors/error_general.php
@@ -2,6 +2,8 @@
 
 $header = file_get_contents(__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR.'templates'.DIRECTORY_SEPARATOR.'header.php');
 $menu = '';
+$ajustaAncho = '';
+$js = '';
 $content = '<h2>'.$heading.'</h2>
 		<div class="error">'.(empty($message) ? $message : $heading).'</div>';
 $footer = file_get_contents(__DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'views'.DIRECTORY_SEPARATOR.'templates'.DIRECTORY_SEPARATOR.'footer.php');;
