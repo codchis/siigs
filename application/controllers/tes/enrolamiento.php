4d8bbb3aa07f27676533f3e266f30639fefa167c
checherman <geminis_che@hotmail.com>
2014-01-14 14:27:01 -0600

    modificar print card

--- application/controllers/tes/enrolamiento.php
+++ application/controllers/tes/enrolamiento.php
@@ -75,7 +75,7 @@ class Enrolamiento extends CI_Controller
 		$alergia=$this->Enrolamiento_model->getAlergia($id);
 		//$persona = (array) $persona;
 		//var_dump($persona);
-		$datos=array("nombre"=>$persona->nombre." ".$persona->apellido_paterno." ".$persona->apellido_materno,
+		$datos=array("nombre"=>$persona->apellido_paterno." ".$persona->apellido_materno." ".$persona->nombre,
 					 "sexo"=>$persona->sexo,
 					 "nombre_madre"=>$persona->nombreT." ".$persona->paternoT." ".$persona->maternoT,
 					 "domicilio"=>$persona->calle_domicilio." ".$persona->numero_domicilio.", ".$persona->colonia_domicilio
@@ -84,11 +84,11 @@ class Enrolamiento extends CI_Controller
 		$asu=$this->ArbolSegmentacion_model->getDescripcionById(array($persona->id_asu_um_tratante),3);// um juridiccion
 		$dom=explode(",",$dom[0]->descripcion);
 		$asu=explode(",",$asu[0]->descripcion);
-		$datos["localidad"]=$dom[0];
-		$datos["municipio"]=$dom[1];
+		$datos["localidad"]=trim($dom[0]);
+		$datos["municipio"]=trim($dom[1]);
 		
-		$datos["um"]=$asu[0];
-		$datos["juridiccion"]=$asu[3];
+		$datos["um"]=trim($asu[0]);
+		$datos["juridiccion"]=trim($asu[3]);
 		$valor=array();
 		foreach($alergia as $x)
 		{
